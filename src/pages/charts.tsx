e83bbd20118628235d8a3a9884149573d9c18d30
Logan Bready <lb@buildmidwestern.com>
2023-01-20 09:25:26 -0600

    feat: added left main navigation to repo.  styles for desktop and mobile

--- src/pages/charts.tsx
+++ src/pages/charts.tsx
@@ -3,7 +3,12 @@ import { LeftMainNavigation } from '@molecules/LeftMainNavigation';
 const Page = () => {
   return (
     <>
-      <LeftMainNavigation />
+      <LeftMainNavigation
+        anchorSide='left'
+        navigationWidth={80}
+        desktopBackgroundColor='#000'
+        mobileBackgroundColor='#fff'
+      />
       <div>
         <h1>Page</h1>
       </div>
