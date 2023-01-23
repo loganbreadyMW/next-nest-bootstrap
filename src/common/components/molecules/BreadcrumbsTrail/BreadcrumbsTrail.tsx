38cf35595ad59003afd9ed7bfc58bc09fbd12e1f
Logan Bready <lb@buildmidwestern.com>
2023-01-23 11:18:07 -0600

    style: fixing styles on the list items for breadcrumbs

--- src/common/components/molecules/BreadcrumbsTrail/BreadcrumbsTrail.tsx
+++ src/common/components/molecules/BreadcrumbsTrail/BreadcrumbsTrail.tsx
@@ -2,7 +2,9 @@ import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Link from '@mui/material/Link';
 import Stack from '@mui/material/Stack';
 import { useRouter } from 'next/router';
+import styles from './BreadcrumbsTrail.module.scss';
 import { ChevronRight } from './ChevronRight';
+import { HomeIcon } from './HomeIcon';
 
 interface BreadcrumbsTrailProps {
   something?: string;
@@ -16,9 +18,20 @@ export const BreadcrumbsTrail = ({ something }: BreadcrumbsTrailProps) => {
     .map((x) => x.replace(/[^a-zA-Z0-9]/g, ' '));
   return (
     <Stack spacing={2}>
-      <Breadcrumbs separator={<ChevronRight />} aria-label='breadcrumb'>
+      <Breadcrumbs
+        separator={<ChevronRight />}
+        aria-label='breadcrumb'
+        sx={{
+          '& .MuiBreadcrumbs-li': {
+            '&:last-of-type': {
+              backgroundColor: '#f2f4f7',
+              padding: '4px 8px',
+            },
+          },
+        }}
+      >
         <Link underline='hover' key='1' color='inherit' href='/'>
-          Home
+          <HomeIcon className={styles['home-icon']} />
         </Link>
         {pathname.map((x) => {
           return (
