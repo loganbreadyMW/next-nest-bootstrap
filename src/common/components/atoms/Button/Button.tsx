4d23ac91cf27930f0d5c19afa1ddf73bc549a939
Logan Bready <lb@buildmidwestern.com>
2023-01-24 12:13:36 -0600

    feat: added button component for add client on action bar

--- src/common/components/atoms/Button/Button.tsx
+++ src/common/components/atoms/Button/Button.tsx
@@ -1,17 +1,16 @@
-import clsx from 'clsx';
-import styles from './Button.module.scss';
-
 interface ButtonProps {
   children: React.ReactNode;
   color?: 'default' | 'primary' | 'secondary' | 'success' | 'danger' | 'warning' | 'info';
   disabled?: boolean;
   onClick?: () => void;
-  variant?: 'contained' | 'outlined' | 'text';
+  variant?: 'contained' | 'outlined' | 'text' | 'filter';
   href?: string;
   size?: 'small' | 'medium' | 'large';
+  className?: string;
 }
 
 export const Button = ({
+  className,
   children,
   color = 'default',
   disabled,
@@ -20,24 +19,16 @@ export const Button = ({
   href,
   size = 'medium',
 }: ButtonProps) => {
-  const buttonClasses = clsx(
-    styles['button__base'],
-    styles[`button__${variant}`],
-    styles[`button__${variant}--${color}`],
-    styles[`button__${variant}--${size}`],
-    disabled && styles[`button__${variant}--disabled`],
-  );
-
   if (href) {
     return (
-      <a href={href} className={buttonClasses}>
+      <a href={href} className={className}>
         {children}
       </a>
     );
   }
 
   return (
-    <button className={buttonClasses} disabled={disabled} onClick={onClick} type='button'>
+    <button className={className} disabled={disabled} onClick={onClick} type='button'>
       {children}
     </button>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
+import { Button } from '@atoms/Button';
+import styles from '@atoms/Button/Button.module.scss';
+import { PlusIcon } from '@atoms/Icons/PlusIcon';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
-  const filterOptions = ['All', 'Active', 'Completed'];
   return (
     <>
-      <div style={{ height: '100vh', width: '100vw' }}></div>
+      <div style={{ height: '100vh', width: '100vw', padding: '10px' }}>
+        <Button
+          color='secondary'
+          variant='filter'
+          className={styles['button__filter']}
+          onClick={() => alert('Clicked')}
+        >
+          <PlusIcon />
+          Add Client
+        </Button>
+      </div>
     </>
   );
 };
