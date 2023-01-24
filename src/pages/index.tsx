d03c0437beeb4e468b72274b73e82b4ec3c010ac
Logan Bready <lb@buildmidwestern.com>
2023-01-24 10:27:02 -0600

    feat: added filter option component for reuse

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
-import { Filter } from '@atoms/Filter';
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   const filterOptions = ['All', 'Active', 'Completed'];
   return (
     <>
-      <div style={{ height: '100vh', width: '100vw' }}>
-        <Filter options={filterOptions} />
-      </div>
+      <div style={{ height: '100vh', width: '100vw' }}></div>
     </>
   );
 };
