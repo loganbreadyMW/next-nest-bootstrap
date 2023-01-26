cd964c7d45e17b45eef1c153e430ffd8f838120c
Logan Bready <lb@buildmidwestern.com>
2023-01-26 08:28:33 -0600

    feat: adding dashboard grid component.  still wip'

--- src/common/components/organisms/DashboardGrid/DashboardGrid.stories.tsx
+++
@@ -1,9 +0,0 @@
-import type { ComponentMeta } from '@storybook/react';
-import { DashboardGrid } from './DashboardGrid';
-
-export default {
-  title: 'organisms/DashboardGrid',
-  component: DashboardGrid,
-} as ComponentMeta<typeof DashboardGrid>;
-
-export const Default = () => <DashboardGrid />;
