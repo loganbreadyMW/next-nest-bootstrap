cc23d1f0a583220b4200cb9da7988fae57475705
Logan Bready <lb@buildmidwestern.com>
2023-01-26 08:16:41 -0600

    feat: adding dashboard grid component.  still wip'

---
+++ src/common/components/molecules/DashboardGridTeam/index.ts
@@ -0,0 +1,1 @@
+export { DashboardGridTeam } from './DashboardGridTeam';
--- src/common/components/organisms/DashboardGrid/DashboardGrid.tsx
+++ src/common/components/organisms/DashboardGrid/DashboardGrid.tsx
@@ -1,5 +1,8 @@
 import type { GridColDef } from '@mui/x-data-grid';
-import { DataGrid } from '@mui/x-data-grid';
+
+interface DashboardGridProps {
+  props: any;
+}
 
 const columns: GridColDef[] = [
   { field: 'id', headerName: 'ID', width: 70 },
@@ -17,91 +20,14 @@ const columns: GridColDef[] = [
     headerName: 'End Date',
     width: 160,
   },
-];
-
-const rows = [
   {
-    id: 1,
-    client: 'apple',
-    estCloseDate: 'Snow',
-    roles: 'Jon',
-    startDate: 35,
-    endDate: '24 Jan  2022',
-  },
-  {
-    id: 2,
-    client: 'apple',
-    estCloseDate: 'Lannister',
-    roles: 'Cersei',
-    startDate: 42,
-    endDate: '24 Jan  2022',
-  },
-  {
-    id: 3,
-    client: 'apple',
-    estCloseDate: 'Lannister',
-    roles: 'Jaime',
-    startDate: 45,
-    endDate: '24 Jan  2022',
-  },
-  {
-    id: 4,
-    client: 'apple',
-    estCloseDate: 'Stark',
-    roles: 'Arya',
-    startDate: 16,
-    endDate: '24 Jan  2022',
-  },
-  {
-    id: 5,
-    client: 'apple',
-    estCloseDate: 'Targaryen',
-    roles: 'Daenerys',
-    startDate: null,
-    endDate: '24 Jan  2022',
-  },
-  {
-    id: 6,
-    client: 'apple',
-    estCloseDate: 'Melisandre',
-    roles: null,
-    startDate: 150,
-    endDate: '24 Jan  2022',
-  },
-  {
-    id: 7,
-    client: 'apple',
-    estCloseDate: 'Clifford',
-    roles: 'Ferrara',
-    startDate: 44,
-    endDate: '24 Jan  2022',
-  },
-  {
-    id: 8,
-    client: 'apple',
-    estCloseDate: 'Frances',
-    roles: 'Rossini',
-    startDate: 36,
-    endDate: '24 Jan  2022',
-  },
-  {
-    id: 9,
-    client: 'apple',
-    estCloseDate: 'Roxie',
-    roles: 'Harvey',
-    startDate: 65,
-    endDate: '24 Jan  2022',
+    field: 'team',
+    headerName: 'Team',
+    width: 160,
   },
 ];
 
-export const DashboardGrid = () => {
-  return (
-    <DataGrid
-      rows={rows}
-      columns={columns}
-      pageSize={5}
-      rowsPerPageOptions={[5]}
-      checkboxSelection
-    />
-  );
+export const DashboardGrid = ({ props }: DashboardGridProps) => {
+  return '';
+  // <DataGrid rows='' columns={columns} pageSize={5} rowsPerPageOptions={[5]} checkboxSelection />
 };
